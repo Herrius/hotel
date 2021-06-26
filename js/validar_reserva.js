903fddffc15d1d01838d8f9b984b668c7d5b0190
Enrique Ubaldo <65200014+Herrius@users.noreply.github.com>
2021-06-26 10:42:25 -0500

    actualizacion

--- js/validar_reserva.js
+++ js/validar_reserva.js
@@ -16,30 +16,37 @@ function validar() {
         alert("Es necesario que rellene todos los campos obligatorios que estan con este simbolo (*)")
         return false;
     }
-    else if(expnombre.test(nombre)){
-        alert("La celda nombres no acepta números ni caracteres especiales")
-        return false;
-    }
-    else if(expnombre.test(apellido)){
-        alert("La celda apellido no acepta números ni caracteres especiales")
-        return false;
-    }
-    else if(expcorreo.test(correo)){
-        alert("El correo no es valido")
-        return false;
-    }
-    else if(correo!==concorreo){
-        alert("La confirmacion del correo debe ser igual al registrado anteriormente")
-        return false;
-    }
-    else if(expcorreo.test(correocliente)){
-        alert("El correo no es valido")
-        return false;
-    }
-    else if(expnombre.test(nombrecliente)){
-        alert("El nombre no acepta números ni caracteres especiales")
-        return false;
-    }
+    else{
+        if(expnombre.test(nombre)){
+            alert("La celda nombres no acepta números ni caracteres especiales",nombre)
+            return false;
+        }
+    } 
+    // else if(expnombre.test(apellido)){
+    //     alert("La celda apellido no acepta números ni caracteres especiales")
+    //     return false;
+    // }
+    // else if(expcorreo.test(correo)){
+    //     alert("El correo no es valido")
+    //     return false;
+    // }
+    // else if(correo!==concorreo){
+    //     alert("La confirmacion del correo debe ser igual al registrado anteriormente")
+    //     return false;
+    // }
+    // else if(expcorreo.test(correocliente)){
+    //     alert("El correo no es valido")
+    //     return false;
+    // }
+    // else if(expnombre.test(nombrecliente)){
+    //     alert("El nombre no acepta números ni caracteres especiales")
+    //     return false;
+    // }
+    
+    
+    
+    
+   
 
 
 }
